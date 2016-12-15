7c9f18f7138ae075a52f321f40e19d697b5e7d27
lasithasenanayake <lasitha.senanayake@gmail.com>
2016-12-15 18:10:15 +0530

    ddos

--- sossconfig/ubuntu.sh
+++ sossconfig/ubuntu.sh
@@ -2,12 +2,9 @@
  apt-get install -y apache2 php5.6 php5.6-curl
  a2enmod rewrite
  service apache2 restart
- 
  debconf-set-selections <<< 'mysql-server mysql-server/root_password password sossgrid'
  debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password sossgrid'
  apt-get -y install mysql-server
-
-
 add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install oracle-java8-installer
